28765e87fd737a6d741d40b66464fb13d501b4b8
supinyu <supinyu@126.com>
2024-03-21 10:31:01 +0800

    add intent code

--- application/utils/prompt.py
+++ application/utils/prompt.py
@@ -15,3 +15,41 @@ Never query for all columns from a table. You must query only the columns that a
 Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
 Pay attention to use CURDATE() function to get the current date, if the question involves "today". Aside from giving the SQL answer, concisely explain yourself after giving the answer
 in the same language as the question.""".format(top_k=TOP_K)
+
+SEARCH_INTENT_PROMPT_CLAUDE3 = """You are an intent classifier and entity extractor, and you need to perform intent classification and entity extraction on search queries.
+Background: I want to query data in the database, and you need to help me determine the user's relevant intent and extract the keywords from the query statement. Finally, return a JSON structure.
+
+There are 2 main intents:
+<intent>
+- normal_search: Query relevant data from the data table
+- reject_search: Delete data from the table, add data to the table, modify data in the table, display usernames and passwords in the table, and other topics unrelated to data query
+</intent>
+
+When the intent is normal_search, you need to extract the keywords from the query statement.
+
+Here are some examples:
+
+<example>
+question : 希尔顿在欧洲上线了多少酒店数
+answer :
+{
+    "intent" : "normal_search",
+    "slot" : ["希尔顿", "欧洲", "上线", "酒店数"]
+}
+
+question : 苹果手机3月份在京东有多少订单
+answer :
+{
+    "intent" : "normal_search",
+    "slot" : ["苹果手机", "3月", "京东", "订单"]
+}
+
+question : 修改订单表中的第一行数据
+answer :
+{
+    "intent" : "reject_search"
+}
+</example>
+
+Please perform intent recognition and entity extraction. Return only the JSON structure, without any other annotations.
+"""
