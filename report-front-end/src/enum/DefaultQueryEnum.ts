be60e7f9c3149a96abef470ccc8e82224ad64452
mwangy <mwangy@amazon.com>
2024-05-07 18:27:50 +0800

    feat: login & config

--- report-front-end/src/enum/DefaultQueryEnum.ts
+++ report-front-end/src/enum/DefaultQueryEnum.ts
@@ -1,1 +1,11 @@
-export const DEFAULT_QUERY_CONFIG = {};
+export const DEFAULT_QUERY_CONFIG = {
+  selectedLLM: "shopping-demo",
+  selectedDataPro: "anthropic.claude-3-opus-20240229-v1:0",
+  intentChecked: false,
+  complexChecked: false,
+  modelSuggestChecked: false,
+  temperature: 0.9,
+  topP: 0.999,
+  topK: 250,
+  maxLength: 2048,
+};
