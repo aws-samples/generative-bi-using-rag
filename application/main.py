9453f7946ca8540a22f78ede4f286ce2deca1c1a
supinyu <supinyu@126.com>
2024-05-08 09:23:48 +0800

    add test api

--- application/main.py
+++ application/main.py
@@ -3,8 +3,20 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from api.exception_handler import biz_exception
 from api.main import router
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI(title='GenBI')
+
 
 app = FastAPI(title='GenBI')
+
+# 配置CORS中间件
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],  # 允许所有源访问，可以根据需求进行修改
+    allow_credentials=True,  # 允许发送凭据（如Cookie）
+    allow_methods=['*'],  # 允许所有HTTP方法
+    allow_headers=['*'],  # 允许所有请求头
+)
 
 # Global exception capture
 biz_exception(app)
