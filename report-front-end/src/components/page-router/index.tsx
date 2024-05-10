4b6add247feec43661e89a41f86714fe1181c1a5
mwangy <mwangy@amazon.com>
2024-05-10 16:29:13 +0800

    feat: upload nginx & docker file

--- report-front-end/src/components/page-router/index.tsx
+++ report-front-end/src/components/page-router/index.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageLayout from "../page-layout";
-import TableList from "../table-list";
-import ChartPage from "../chart-page";
 import Chat from "../chatbot/chat";
 
 const PageRouter = () => {
@@ -9,14 +7,6 @@ const PageRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PageLayout content={<Chat></Chat>} />} />
-        <Route
-          path="/page1"
-          element={<PageLayout content={<TableList></TableList>} />}
-        />
-        <Route
-          path="/page2"
-          element={<PageLayout content={<ChartPage></ChartPage>} />}
-        />
       </Routes>
     </BrowserRouter>
   );
