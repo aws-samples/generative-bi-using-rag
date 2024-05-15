dce9aebef300ac8a8199012b67bcce335dd3d3ec
supinyu <supinyu@126.com>
2024-05-15 20:20:34 +0800

    change streamlit show

--- application/pages/1_🌍_Generative_BI_Playground.py
+++ application/pages/1_🌍_Generative_BI_Playground.py
@@ -11,13 +11,15 @@ from nlq.business.connection import ConnectionManagement
 from nlq.business.nlq_chain import NLQChain
 from nlq.business.profile import ProfileManagement
 from nlq.business.vector_store import VectorStore
+from utils.domain import SearchTextSqlResult
 from utils.llm import get_query_intent, generate_suggested_question, get_agent_cot_task, data_analyse_tool, \
-    knowledge_search
+    knowledge_search, text_to_sql
 from utils.navigation import make_sidebar
 from utils.apis import get_sql_result_tool
 
 from utils.opensearch import get_retrieve_opensearch
 from utils.text_search import normal_text_search, agent_text_search
+from utils.tool import get_generated_sql
 
 logger = logging.getLogger(__name__)
 
@@ -104,6 +106,78 @@ def recurrent_display(messages, i, current_nlq_chain):
     return i
 
 
+def normal_text_search_streamlit(search_box, model_type, database_profile, entity_slot, env_vars, selected_profile, use_rag,
+                       model_provider=None):
+    entity_slot_retrieve = []
+    retrieve_result = []
+    response = ""
+    sql = ""
+    search_result = SearchTextSqlResult(search_query=search_box, entity_slot_retrieve=entity_slot_retrieve,
+                                        retrieve_result=retrieve_result, response=response, sql=sql)
+    try:
+        if database_profile['db_url'] == '':
+            conn_name = database_profile['conn_name']
+            db_url = ConnectionManagement.get_db_url_by_name(conn_name)
+            database_profile['db_url'] = db_url
+            database_profile['db_type'] = ConnectionManagement.get_db_type_by_name(conn_name)
+
+        with st.status("Performing Entity retrieval...") as status_text:
+            if len(entity_slot) > 0 and use_rag:
+                for each_entity in entity_slot:
+                    entity_retrieve = get_retrieve_opensearch(env_vars, each_entity, "ner",
+                                                              selected_profile, 1, 0.7)
+                    if len(entity_retrieve) > 0:
+                        entity_slot_retrieve.extend(entity_retrieve)
+            examples = []
+            for example in entity_slot_retrieve:
+                examples.append({'Score': example['_score'],
+                                 'Question': example['_source']['entity'],
+                                 'Answer': example['_source']['comment'].strip()})
+            st.write(examples)
+            status_text.update(
+                label=f"Entity Retrieval Completed: {len(entity_slot_retrieve)} entities retrieved",
+                state="complete", expanded=False)
+
+        with st.status("Performing QA retrieval...") as status_text:
+            if use_rag:
+                retrieve_result = get_retrieve_opensearch(env_vars, search_box, "query",
+                                                      selected_profile, 3, 0.5)
+                examples = []
+                for example in retrieve_result:
+                    examples.append({'Score': example['_score'],
+                                     'Question': example['_source']['text'],
+                                     'Answer': example['_source']['sql'].strip()})
+                st.write(examples)
+                status_text.update(
+                    label=f"QA Retrieval Completed: {len(retrieve_result)} entities retrieved",
+                    state="complete", expanded=False)
+
+        with st.status("Generating SQL... ") as status_text:
+            response = text_to_sql(database_profile['tables_info'],
+                               database_profile['hints'],
+                               database_profile['prompt_map'],
+                               search_box,
+                               model_id=model_type,
+                               sql_examples=retrieve_result,
+                               ner_example=entity_slot_retrieve,
+                               dialect=database_profile['db_type'],
+                               model_provider=model_provider)
+
+            status_text.update(
+                    label=f"Generating SQL Done",
+                    state="complete", expanded=False)
+
+            sql = get_generated_sql(response)
+            search_result = SearchTextSqlResult(search_query=search_box, entity_slot_retrieve=entity_slot_retrieve,
+                                                retrieve_result=retrieve_result, response=response, sql="")
+            search_result.entity_slot_retrieve = entity_slot_retrieve
+            search_result.retrieve_result = retrieve_result
+            search_result.response = response
+            search_result.sql = sql
+    except Exception as e:
+        logger.error(e)
+    return search_result
+
 def main():
     load_dotenv()
 
@@ -180,6 +254,7 @@ def main():
         intent_ner_recognition_flag = st.checkbox("Intent NER", True)
         agent_cot_flag = st.checkbox("Agent COT", True)
         explain_gen_process_flag = st.checkbox("Explain Generation Process", True)
+        data_with_analyse = st.checkbox("Data With Analyse", False)
         gen_suggested_question_flag = st.checkbox("Generate Suggested Questions", False)
 
         clean_history = st.button("clean history", on_click=clean_st_history, args=[selected_profile])
@@ -272,6 +347,7 @@ def main():
                         intent_response = get_query_intent(model_type, search_box, prompt_map)
                         intent = intent_response.get("intent", "normal_search")
                         entity_slot = intent_response.get("slot", [])
+                        st.write(intent_response)
                         status_text.update(label=f"Intent Recognition Completed: This is a **{intent}** question",
                                            state="complete", expanded=False)
                         if intent == "reject_search":
@@ -293,16 +369,13 @@ def main():
                 else:
                     search_intent_flag = True
 
-                with st.expander('Query Intent Response'):
-                    st.write(intent_response)
-
                 # 主要的逻辑部分，调用LLM
                 if reject_intent_flag:
                     st.write("Your query statement is currently not supported by the system")
 
                 elif search_intent_flag:
                     # 执行普通的查询，并可视化结果
-                    normal_search_result = normal_text_search(search_box, model_type,
+                    normal_search_result = normal_text_search_streamlit(search_box, model_type,
                                                               database_profile,
                                                               entity_slot, env_vars,
                                                               selected_profile,
@@ -336,29 +409,29 @@ def main():
                 # 前端结果显示agent cot任务拆分信息, normal_text_search 的显示，做了拆分，为了方便跟API逻辑一致
                 if search_intent_flag:
                     entity_slot_retrieve = normal_search_result.entity_slot_retrieve
-                    if use_rag_flag:
-                        with st.status("Performing QA retrieval...") as status_text:
-                            examples = []
-                            for example in entity_slot_retrieve:
-                                examples.append({'Score': example['_score'],
-                                                 'Question': example['_source']['entity'],
-                                                 'Answer': example['_source']['comment'].strip()})
-                            st.write(examples)
-                            status_text.update(
-                                label=f"Entity Retrieval Completed: {len(entity_slot_retrieve)} entities retrieved",
-                                state="complete", expanded=False)
-                    retrieve_result = normal_search_result.retrieve_result
-                    if use_rag_flag:
-                        with st.status("Performing QA retrieval...") as status_text:
-                            examples = []
-                            for example in retrieve_result:
-                                examples.append({'Score': example['_score'],
-                                                 'Question': example['_source']['text'],
-                                                 'Answer': example['_source']['sql'].strip()})
-                            st.write(examples)
-                            status_text.update(
-                                label=f"QA Retrieval Completed: {len(retrieve_result)} entities retrieved",
-                                state="complete", expanded=False)
+                    # if use_rag_flag:
+                    #     with st.status("Performing QA retrieval...") as status_text:
+                    #         examples = []
+                    #         for example in entity_slot_retrieve:
+                    #             examples.append({'Score': example['_score'],
+                    #                              'Question': example['_source']['entity'],
+                    #                              'Answer': example['_source']['comment'].strip()})
+                    #         st.write(examples)
+                    #         status_text.update(
+                    #             label=f"Entity Retrieval Completed: {len(entity_slot_retrieve)} entities retrieved",
+                    #             state="complete", expanded=False)
+                    # retrieve_result = normal_search_result.retrieve_result
+                    # if use_rag_flag:
+                    #     with st.status("Performing QA retrieval...") as status_text:
+                    #         examples = []
+                    #         for example in retrieve_result:
+                    #             examples.append({'Score': example['_score'],
+                    #                              'Question': example['_source']['text'],
+                    #                              'Answer': example['_source']['sql'].strip()})
+                    #         st.write(examples)
+                    #         status_text.update(
+                    #             label=f"QA Retrieval Completed: {len(retrieve_result)} entities retrieved",
+                    #             state="complete", expanded=False)
                     if normal_search_result.sql != "":
                         current_nlq_chain.set_generated_sql(normal_search_result.sql)
                         st.code(normal_search_result.sql, language="sql")
@@ -403,7 +476,7 @@ def main():
                         with st.expander("The SQL Error Info"):
                             st.markdown(search_intent_result["error_info"])
                     else:
-                        if search_intent_result["data"] is not None and len(search_intent_result["data"]) > 0:
+                        if search_intent_result["data"] is not None and len(search_intent_result["data"]) > 0 and data_with_analyse:
                             with st.spinner('Generating data summarize...'):
                                 search_intent_analyse_result = data_analyse_tool(model_type, prompt_map, search_box,
                                                                                  search_intent_result["data"].to_json(
