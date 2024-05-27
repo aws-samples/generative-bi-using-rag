f251ceed033f832cd5b57f5f4b6b2550689d5335
wubinbin <wubinbin@amazon.com>
2024-05-27 16:57:41 +0800

    Add a right logo

--- report-front-end/src/components/top-navigation/index.tsx
+++ report-front-end/src/components/top-navigation/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { TopNavigation } from "@cloudscape-design/components";
 import "./style.scss"
-import { APP_LOGO, APP_TITLE } from "../../tools/const";
+import { APP_LOGO, APP_RIGHT_LOGO, APP_TITLE } from "../../tools/const";
 
 const CustomTopNavigation = () => {
 
   return (
     <div className="top-nav" id="awsui-top-navigation">
+      {APP_RIGHT_LOGO && (
+        <img className="logo" src={APP_RIGHT_LOGO} alt="logo"/>
+      )
+      }
       <TopNavigation
         identity={{
           href: "#",
