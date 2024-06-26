2a00c87bf5038b93dbe1597c4532bb51c31a5c06
Zhoutong Wang <wzt1001@hotmail.com>
2024-06-26 11:15:02 +0000

    changed env to runtime

--- report-front-end/src/pages/login-page/aws-config.ts
+++ report-front-end/src/pages/login-page/aws-config.ts
@@ -1,7 +1,7 @@
 export const awsConfig = {
     Auth: {
-        region: import.meta.env.VITE_COGNITO_REGION,
-        userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-        userPoolWebClientId: import.meta.env.VITE_COGNITO_USER_POOL_WEB_CLIENT_ID,
+        region: process.env.VITE_COGNITO_REGION,
+        userPoolId: process.env.VITE_COGNITO_USER_POOL_ID,
+        userPoolWebClientId: process.env.VITE_COGNITO_USER_POOL_WEB_CLIENT_ID,
     }
 };
