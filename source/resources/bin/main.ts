ad09fabe49f9f94db2b99384d3b033485c3c0efa
Zhoutong Wang <wzt1001@hotmail.com>
2024-07-01 15:37:31 +0000

    stack edited

--- source/resources/bin/main.ts
+++ source/resources/bin/main.ts
@@ -1,11 +1,13 @@
-#!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { MainStack } from '../lib/main-stack';
+
 const devEnv = {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
 };
 
 const app = new cdk.App();
-new MainStack(app, 'GenBiMainStack', { env: devEnv });
+const deployRds = process.argv.includes('--deploy-rds'); // Check if --deploy-rds flag is present
+
+new MainStack(app, 'GenBiMainStack', { env: devEnv, deployRds }); // Pass deployRDS flag to MainStack constructor
 app.synth();
