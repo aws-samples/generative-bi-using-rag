063c8b70f8c79761be5d92ca8576316eb6a6afc8
Zhoutong Wang <wzt1001@hotmail.com>
2024-07-02 08:05:15 +0000

    vpc added

--- report-front-end/docker-entry.sh
+++ report-front-end/docker-entry.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-echo "Injecting env vars into Vite's static files"
-# replacing Vite's static env vars with injected one
-vars=$(printenv | grep '^VITE_' | awk -F= '{print $1}')
+# Read variable names from the .env file
+env_file="/.env"
+vars="VITE_COGNITO_REGION
+VITE_COGNITO_USER_POOL_ID
+VITE_COGNITO_USER_POOL_WEB_CLIENT_ID
+VITE_BACKEND_URL
+VITE_WEBSOCKET_URL"
+
+# Iterate through .js files in /usr/share/nginx/html and replace variables
 find "/usr/share/nginx/html" -type f -name "*.js" | while read file; do
     for var in $vars; do
-        echo "Replacing $var in $file"
-        sed -i "s/\($var:\"\)[^\"]*\"/\1$(printenv "$var")\"/g" $file
+        placeholder="PLACEHOLDER_$var"
+        value=$(printenv $var)
+        echo "Replacing $placeholder with $value"
+        sed -i "s/$placeholder/$value/g" $file
     done
-done
-
-# Replace region, userPoolId, and userPoolWebClientId with VITE_COGNITO_REGION, VITE_COGNITO_USER_POOL_ID, and VITE_COGNITO_USER_POOL_WEB_CLIENT_ID values
-find "/usr/share/nginx/html" -type f -name "*.js" | while read file; do
-    echo "Replacing Cognito values in $file"
-    sed -i 's/{region:"[^"]*",userPoolId:"[^"]*",userPoolWebClientId:"[^"]*"}\?/{region:"'"$VITE_COGNITO_REGION"'",userPoolId:"'"$VITE_COGNITO_USER_POOL_ID"'",userPoolWebClientId:"'"$VITE_COGNITO_USER_POOL_WEB_CLIENT_ID"'"}'/g "$file"
 done
 
 exec "$@"
