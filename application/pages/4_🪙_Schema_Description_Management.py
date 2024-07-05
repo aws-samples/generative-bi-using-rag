ffae9554fce1de01280b0d9a3c67a7f7cb47f17a
Zhoutong Wang <wzt1001@hotmail.com>
2024-07-05 04:28:40 +0000

    USE CREATE table

--- application/pages/4_🪙_Schema_Description_Management.py
+++ application/pages/4_🪙_Schema_Description_Management.py
@@ -41,32 +41,42 @@ def main():
                 st.caption(f'Table description: {table_desc}')
                 tbl_annotation = st.text_input('Table annotation', table_anno)
 
-                if column_anno is not None:
-                    col_annotation_text = column_anno
-                else:
-                # st.write(table_ddl)
-                    print(table_ddl)
-                    col_annotation_text = ''
-                    start_flag = False
-                    for li in table_ddl.split('\n'):
-                        li = li.strip()
-                        if li[0] == '(':
-                            start_flag = True
-                            continue
-                        if li[0] == ')':
-                            start_flag = False
-                            continue
-                        if start_flag:
-                            li_parts = li.split(' ')
-                            col_name = li_parts[0]
-                            col_type = li_parts[1]
-                            print(li_parts[2:])
-                            col_comment = ' '.join(li_parts[2:]).replace(',', '').replace('--', '')
-                            # print(li.split(' '))
-                            col_annotation_text += f'- name: {col_name}, datatype: {col_type}, comment: {col_comment}\n'
-                            col_annotation_text += '  annotation: \n'
-
-                col_annotation = st.text_area('Column annotation', col_annotation_text, height=500)
+                st.caption('''CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY, # Unique identifier for each employee
+    name VARCHAR(100) NOT NULL, # Employee name
+    position VARCHAR(50) NOT NULL, # Job position, 2 possible values: 'Engineer', 'Manager'
+    salary DECIMAL(10, 2)  # Salary in USD, e.g., 1000.00
+    date VARCHAR(10) NOT NULL, # Date of joining the company, format 'YYYY-MM-DD'
+    ...
+);
+''')
+                # if column_anno is not None:
+                #     col_annotation_text = column_anno
+                # else:
+                # # st.write(table_ddl)
+                #     print(table_ddl)
+                #     col_annotation_text = ''
+                #     start_flag = False
+                #     for li in table_ddl.split('\n'):
+                #         li = li.strip()
+                #         if li[0] == '(':
+                #             start_flag = True
+                #             continue
+                #         if li[0] == ')':
+                #             start_flag = False
+                #             continue
+                #         if start_flag:
+                #             li_parts = li.split(' ')
+                #             col_name = li_parts[0]
+                #             col_type = li_parts[1]
+                #             print(li_parts[2:])
+                #             col_comment = ' '.join(li_parts[2:]).replace(',', '').replace('--', '')
+                #             # print(li.split(' '))
+                #             col_annotation_text += f'- name: {col_name}, datatype: {col_type}, comment: {col_comment}\n'
+                #             col_annotation_text += '  annotation: \n'
+                
+                # col_annotation = st.text_area('Column annotation', col_annotation_text, height=500)
+                col_annotation = st.text_area('Column annotation', table_ddl, height=400)
 
                 if st.button('Save', type='primary'):
                     origin_tables_info = profile_detail.tables_info
