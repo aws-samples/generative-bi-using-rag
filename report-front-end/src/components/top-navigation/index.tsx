b6491b4170ec2c76bb086b36814e53cf7e21ee6a
Peiwen Li <impw@amazon.com>
2024-07-08 11:46:06 +0800

    ui-improve: TOP NAV: add login default and loading text etc; temp disable dark mode to alleviate workload and user not required at this stage; and other general style improvements

--- report-front-end/src/components/top-navigation/index.tsx
+++ report-front-end/src/components/top-navigation/index.tsx
@@ -2,19 +2,20 @@ import {
   ButtonDropdownProps,
   TopNavigation,
 } from "@cloudscape-design/components";
-import { Mode } from "@cloudscape-design/global-styles";
-import { useEffect, useState } from "react";
-import { Storage } from "../../common/helpers/storage";
+// import { Mode } from '@cloudscape-design/global-styles'
 import { Auth } from "aws-amplify";
-import { APP_LOGO, APP_RIGHT_LOGO, APP_TITLE, CHATBOT_NAME } from "../../common/constant/constants";
+import { useEffect, useState } from "react";
+import { APP_RIGHT_LOGO, APP_TITLE } from "../../common/constant/constants";
+// import { Storage } from '../../common/helpers/storage'
 import "./style.scss";
 
 export default function CustomTopNavigation() {
-  const [userName, setUserName] = useState<string | null>(null);
-  const [theme, setTheme] = useState<Mode>(Storage.getTheme());
+  const [userName, setUserName] = useState<string>("Authenticating");
+  const [userEmail, setUserEmail] = useState<string>("Authenticating");
+  // const [theme, setTheme] = useState<Mode>(Storage.getTheme())
 
   useEffect(() => {
-    (async () => {
+    async function getUser() {
       const result = await Auth.currentUserInfo();
 
       if (!result || Object.keys(result).length === 0) {
@@ -22,18 +23,19 @@ export default function CustomTopNavigation() {
         return;
       }
 
-      const userName = result?.attributes?.email;
-      setUserName(userName);
-    })();
+      setUserEmail(result?.attributes?.email || "Not logged in");
+      setUserName(result?.username || "Not logged in");
+    }
+    getUser();
   }, []);
 
-  const onChangeThemeClick = () => {
-    if (theme === Mode.Dark) {
-      setTheme(Storage.applyTheme(Mode.Light));
-    } else {
-      setTheme(Storage.applyTheme(Mode.Dark));
-    }
-  };
+  // const onChangeThemeClick = () => {
+  //   if (theme === Mode.Dark) {
+  //     setTheme(Storage.applyTheme(Mode.Light))
+  //   } else {
+  //     setTheme(Storage.applyTheme(Mode.Dark))
+  //   }
+  // }
 
   const onUserProfileClick = ({
     detail,
@@ -50,25 +52,24 @@ export default function CustomTopNavigation() {
       style={{ zIndex: 1002, top: 0, left: 0, right: 0, position: "fixed" }}
       id="awsui-top-navigation"
     >
-      {APP_RIGHT_LOGO && (<img className="logo" src={APP_RIGHT_LOGO} alt="logo"/>)}
+      {APP_RIGHT_LOGO && (
+        <img className="logo" src={APP_RIGHT_LOGO} alt="logo" />
+      )}
       <TopNavigation
         identity={{
           href: "/",
           title: APP_TITLE,
-          logo: {
-            src: APP_LOGO,
-            alt: { CHATBOT_NAME } + " Logo"
-          },
         }}
         utilities={[
-          {
-            type: "button",
-            text: theme === Mode.Dark ? "Light Mode" : "Dark Mode",
-            onClick: onChangeThemeClick,
-          },
+          // {
+          //   type: 'button',
+          //   text: theme === Mode.Dark ? 'Light Mode' : 'Dark Mode',
+          //   onClick: onChangeThemeClick,
+          // },
           {
             type: "menu-dropdown",
-            description: userName ?? "",
+            text: userName,
+            description: userEmail,
             iconName: "user-profile",
             onItemClick: onUserProfileClick,
             items: [
@@ -77,9 +78,6 @@ export default function CustomTopNavigation() {
                 text: "Sign out",
               },
             ],
-          },
-          {
-            type: "button",
           },
         ]}
       />
