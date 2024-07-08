2d2fba1e506fd547f6e7f669a85a55f06263ccb5
Peiwen Li <impw@amazon.com>
2024-07-08 11:46:58 +0800

    ui-chore: eslint rule change no-extra-semi off

--- report-front-end/.eslintrc.cjs
+++ report-front-end/.eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+    "no-extra-semi": "off",
   },
 };
