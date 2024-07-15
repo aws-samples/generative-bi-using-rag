cd468a2bb0c5ff6791daf6451db35d97510d3400
root <root@ip-172-31-46-4.ec2.internal>
2024-07-15 02:47:40 +0000

    changes for authentication

--- application/main.py
+++ application/main.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Request, HTTPException
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, Response
 from api.exception_handler import biz_exception
 from api.main import router
 from fastapi.middleware.cors import CORSMiddleware
 from api import service
 from api.schemas import Option
+import requests
+import json
+import base64
 
 app = FastAPI(title='GenBI')
 
@@ -17,6 +20,30 @@ app.add_middleware(
     allow_methods=['*'],  # 允许所有HTTP方法
     allow_headers=['*'],  # 允许所有请求头
 )
+
+validate_url = 'https://apimarket-test.shinho.net.cn/dops-temp/token/validate'
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    print('---HTTP REQUEST---', vars(request), request.cookies)
+    jwt_token = request.cookies.get('jwtToken', None)
+    print('---JWT TOKEN---', jwt_token)
+    if jwt_token:
+        response = requests.post(validate_url, data=jwt_token)
+        if response.status_code != 200 :
+            return Response(status_code=status.HTTP_401_UNAUTHORIZED)
+        else:
+            payload = json.loads(response.text)
+            if payload['data']:
+                msg = json.loads(payload['msg'])
+                response = await call_next(request)
+                response.headers["X-User-Id"] = base64.b64encode(msg['userId'].encode('utf-8')).decode('utf-8')
+                response.headers["X-User-Name"] = base64.b64encode(msg['userName'].encode('utf-8')).decode('utf-8')
+                return response
+            else:
+                return Response(status_code=status.HTTP_401_UNAUTHORIZED)
+    else:
+        return Response(status_code=status.HTTP_401_UNAUTHORIZED)
 
 # Global exception capture
 biz_exception(app)
