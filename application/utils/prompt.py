fdbf90449e107ac4731a669b21339385129ec720
dingtianlu <dingtianlu@shinho.net.cn>
2024-07-16 15:14:16 +0800

    调试clickhouse提示词+建表语句调整

--- application/utils/prompt.py
+++ application/utils/prompt.py
@@ -22,11 +22,12 @@ You are a data analysis expert and proficient in StarRocks. Given an input quest
 question.When generating SQL, do not add double quotes or single quotes around table names. Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per StarRocks SQL. 
 Never query for all columns from a table.""".format(top_k=TOP_K)
 
-
 CLICKHOUSE_DIALECT_PROMPT_CLAUDE3="""
-You are a data analysis expert and proficient in Clickhouse. Given an input question, first create a syntactically correct Clickhouse SQL query to run, then look at the results of the query and return the answer to the input 
-question.When generating SQL, do not add double quotes or single quotes around table names. Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per Clickhouse SQL. 
-Never query for all columns from a table.""".format(top_k=TOP_K)
+You are a data analysis expert and proficient in Clickhouse. Given an input question, first create a syntactically correct Clickhouse query to run, then look at the results of the query and return the answer to the input question.
+Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per ClickHouse. You can order the results to return the most informative data in the database.
+Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
+Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+Pay attention to use today() function to get the current date, if the question involves "today".Pay attention to adapted to the table field type. Please follow the clickhouse syntax or function case specifications.""".format(top_k=TOP_K)
 
 AWS_REDSHIFT_DIALECT_PROMPT_CLAUDE3 = """You are a Amazon Redshift expert. Given an input question, first create a syntactically correct Redshift query to run, then look at the results of the query and return the answer to the input 
 question.When generating SQL, do not add double quotes or single quotes around table names. Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per MySQL. 
