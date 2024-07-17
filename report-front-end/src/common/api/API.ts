45e6ccfd100a664dd47034e15a9c13009a6fa99e
wubinbin <wubinbin@amazon.com>
2024-07-17 19:09:09 +0800

    remove log print

--- report-front-end/src/common/api/API.ts
+++ report-front-end/src/common/api/API.ts
@@ -6,7 +6,6 @@ const instance = axios.create();
 
 instance.interceptors.response.use(
   (response) => {
-    console.log(response);
     if (response.headers && response.headers["x-user-id"]) {
       const patchEvent = new CustomEvent("authorized", {
         detail: {
