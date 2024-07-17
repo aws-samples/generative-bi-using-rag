ea60e5b57c24db07c9feaa1619bba92f6d431772
supinyu <supinyu@126.com>
2024-07-17 11:33:51 +0800

    fix __dirname node

--- source/resources/bin/main.ts
+++ source/resources/bin/main.ts
@@ -2,15 +2,13 @@ import * as cdk from 'aws-cdk-lib';
 import { MainStack } from '../lib/main-stack';
 import * as fs from 'fs';
 import * as path from 'path';
-import { fileURLToPath } from 'url';
 
 const devEnv = {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
 };
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+declare const __dirname: string;
 
 const configPath = path.join(__dirname, '..', 'cdk-config.json');
 const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
