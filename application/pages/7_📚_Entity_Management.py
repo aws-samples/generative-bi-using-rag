a160dd7237d263285839fe3ccb194555514346f1
Pinyu Su <pinyusu@amazon.com>
2024-08-06 11:44:33 +0800

    fix some issue and rollback related code

--- application/pages/7_📚_Entity_Management.py
+++ application/pages/7_📚_Entity_Management.py
@@ -62,8 +62,8 @@ def main():
                                            index=None,
                                            placeholder="Please select data profile...", key='current_profile_name')
 
-    tab_view, tab_add, tab_dimension, tab_search, batch_insert = st.tabs(
-        ['View Entity Info', 'Add Metrics Entity', 'Add Dimension Entity', 'Entity Search', 'Batch Insert Entity'])
+    tab_view, tab_add, tab_search, batch_insert = st.tabs(
+        ['View Samples', 'Add New Sample', 'Sample Search', 'Batch Insert Samples'])
     if current_profile is not None:
         st.session_state['current_profile'] = current_profile
         with tab_view:
@@ -81,7 +81,7 @@ def main():
                 entity = st.text_input('Entity', key='index_question')
                 comment = st.text_area('Comment', key='index_answer', height=300)
 
-                if st.button('Add Metrics Entity', type='primary'):
+                if st.button('Submit', type='primary'):
                     if len(entity) > 0 and len(comment) > 0:
                         VectorStore.add_entity_sample(current_profile, entity, comment)
                         st.success('Sample added')
@@ -91,25 +91,6 @@ def main():
                         st.rerun()
                     else:
                         st.error('please input valid question and answer')
-        with tab_dimension:
-            if current_profile is not None:
-                entity = st.text_input('Entity', key='index_entity')
-                table = st.text_input('Table', key='index_table')
-                column = st.text_input('Column', key='index_column')
-                value = st.text_input('Dimension value', key='index_value')
-                if st.button('Add Dimension Entity', type='primary'):
-                    if len(entity) > 0 and len(table) > 0 and len(column) > 0 and len(value) > 0:
-                        entity_item_table_info = {}
-                        entity_item_table_info["table_name"] = table
-                        entity_item_table_info["column_name"] = column
-                        entity_item_table_info["value"] = value
-                        VectorStore.add_entity_sample(current_profile, entity, "", "dimension", entity_item_table_info)
-                        st.success('Sample added')
-                        time.sleep(2)
-                        st.rerun()
-                    else:
-                        st.error('please input valid question and answer')
-
         with tab_search:
             if current_profile is not None:
                 entity_search = st.text_input('Entity Search', key='index_entity_search')
