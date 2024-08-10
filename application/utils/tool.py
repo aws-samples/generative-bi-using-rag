e63a27f317eedf56942d69d5d7ba49665d436dca
Pinyu Su <pinyusu@amazon.com>
2024-08-10 16:05:43 +0800

    remove python share data

--- application/utils/tool.py
+++ application/utils/tool.py
@@ -3,15 +3,9 @@ import logging
 import time
 import random
 from datetime import datetime
-from multiprocessing import Manager
-
-from api.schemas import Message
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-manager = Manager()
-shared_data = manager.dict()
 
 
 def get_generated_sql(generated_sql_response):
