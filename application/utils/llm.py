bc34bf6ebf3f53d72c2e327312898bfec02f1efc
dingtianlu <dingtianlu@shinho.net.cn>
2024-08-12 14:12:21 +0800

    fix query rewrite bug

--- application/utils/llm.py
+++ application/utils/llm.py
@@ -452,9 +452,9 @@ def get_query_rewrite(model_id, search_box, prompt_map, chat_history):
             user_prompt, system_prompt = generate_query_rewrite_prompt(prompt_map, search_box, model_id, history_query)
             max_tokens = 2048
             final_response = invoke_llm_model(model_id, system_prompt, user_prompt, max_tokens, False)
-            query_rewrite_result = json_parse.parse(final_response)
+            # query_rewrite_result = json_parse.parse(final_response)
             logger.info(f'{final_response=}')
-            query_rewrite['query'] = query_rewrite_result
+            query_rewrite['query'] = final_response
             return query_rewrite
     except Exception as e:
         logger.error("get_query_rewrite is error:{}".format(e))
