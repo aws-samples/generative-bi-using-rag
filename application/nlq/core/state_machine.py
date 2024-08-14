c379f78d5ec2cb9ba042e66094baa8e1d5702da1
Pinyu Su <pinyusu@amazon.com>
2024-08-14 13:51:59 +0800

    add code update

--- application/nlq/core/state_machine.py
+++ application/nlq/core/state_machine.py
@@ -93,100 +93,104 @@ class QueryStateMachine:
                 self.state = QueryState.ERROR
 
     def handle_initial(self):
-        # Initial setup and context preparation
         if self.context.context_window > 0:
-            user_query_history = self.context.user_query_history
-            query_rewrite_result = get_query_rewrite(self.context.model_type, self.context.search_box,
-                                                     self.context.database_profile['prompt_map'], user_query_history)
-            self.context.query_rewrite = query_rewrite_result.get("query")
-            query_rewrite_intent = query_rewrite_result.get("intent")
-            if "ask_in_reply" == query_rewrite_intent:
-                self.answer.query = self.context.search_box
-                self.answer.query_intent = "ask_in_reply"
-                self.answer.query_rewrite = self.context.query_rewrite
-                self.answer.ask_rewrite_result.query_rewrite = self.context.query_rewrite
-                self.transition(QueryState.COMPLETE)
-            else:
-                self.transition(QueryState.INTENT_RECOGNITION)
+            self._handle_query_rewrite()
         else:
             self.transition(QueryState.INTENT_RECOGNITION)
 
-    def handle_entity_retrieval(self):
-        # Perform entity retrieval
-        if self.context.use_rag_flag:
-            self.normal_search_entity_slot = entity_retrieve_search(self.entity_slot,
-                                                                    self.context.opensearch_info,
-                                                                    self.context.selected_profile)
+    def _handle_query_rewrite(self):
+        query_rewrite_result = get_query_rewrite(self.context.model_type, self.context.search_box,
+                                                 self.context.database_profile['prompt_map'],
+                                                 self.context.user_query_history)
+        self.context.query_rewrite = query_rewrite_result.get("query")
+        query_rewrite_intent = query_rewrite_result.get("intent")
+        if query_rewrite_intent == "ask_in_reply":
+            self._set_ask_in_reply_result()
         else:
-            self.normal_search_entity_slot = []
+            self.transition(QueryState.INTENT_RECOGNITION)
 
+    def _set_ask_in_reply_result(self):
+        self.answer.query = self.context.search_box
+        self.answer.query_intent = "ask_in_reply"
+        self.answer.query_rewrite = self.context.query_rewrite
+        self.answer.ask_rewrite_result.query_rewrite = self.context.query_rewrite
+        self.transition(QueryState.COMPLETE)
+
+    def handle_entity_retrieval(self):
+        self.normal_search_entity_slot = self._perform_entity_retrieval()
         self.transition(QueryState.QA_RETRIEVAL)
 
-    def handle_qa_retrieval(self):
-        # Perform QA retrieval
+    def _perform_entity_retrieval(self):
         if self.context.use_rag_flag:
-            self.normal_search_qa_retrival = qa_retrieve_search(self.context.query_rewrite,
-                                                                self.context.opensearch_info,
-                                                                self.context.selected_profile)
-        else:
-            self.normal_search_qa_retrival = []
+            return entity_retrieve_search(self.entity_slot, self.context.opensearch_info, self.context.selected_profile)
+        return []
 
+    def handle_qa_retrieval(self):
+        self.normal_search_qa_retrival = self._perform_qa_retrieval()
         self.transition(QueryState.SQL_GENERATION)
 
-    def handel_entity_select(self):
-        pass
+    def _perform_qa_retrieval(self):
+        if self.context.use_rag_flag:
+            return qa_retrieve_search(self.context.query_rewrite, self.context.opensearch_info,
+                                      self.context.selected_profile)
+        return []
 
     def handle_sql_generation(self):
-        # Generate SQL
-        sql = ""
-        response = ""
+        sql, response = self._generate_sql()
+        self.intent_search_result["sql"] = sql
+        self.intent_search_result["response"] = response
+        if self.context.visualize_results_flag:
+            self.transition(QueryState.EXECUTE_QUERY)
+        else:
+            self.transition(QueryState.COMPLETE)
+
+    def _generate_sql(self):
         try:
-            response = text_to_sql(self.context.database_profile['tables_info'],
-                                   self.context.database_profile['hints'],
-                                   self.context.database_profile['prompt_map'],
-                                   self.context.query_rewrite,
-                                   model_id=self.context.model_type,
-                                   sql_examples=self.normal_search_qa_retrival,
+            response = text_to_sql(self.context.database_profile['tables_info'], self.context.database_profile['hints'],
+                                   self.context.database_profile['prompt_map'], self.context.query_rewrite,
+                                   model_id=self.context.model_type, sql_examples=self.normal_search_qa_retrival,
                                    ner_example=self.normal_search_entity_slot,
-                                   dialect=self.context.database_profile['db_type'],
-                                   model_provider=None)
+                                   dialect=self.context.database_profile['db_type'])
             sql = get_generated_sql(response)
+            return sql, response
         except Exception as e:
             logger.error("handle_sql_generation is error")
             logger.error(e)
-        self.intent_search_result["sql"] = sql
-        self.intent_search_result["response"] = response
-        self.transition(QueryState.EXECUTE_QUERY)
+            return "", ""
 
     def handle_agent_sql_generation(self):
         pass
 
     def handle_intent_recognition(self):
-        # Perform intent recognition
         if self.context.intent_ner_recognition_flag:
             intent_response = get_query_intent(self.context.model_type, self.context.query_rewrite,
                                                self.context.database_profile['prompt_map'])
-            intent = intent_response.get("intent", "normal_search")
-            self.entity_slot = intent_response.get("slot", [])
-            if intent == "reject_search":
-                self.reject_intent_flag = True
+            self._process_intent_response(intent_response)
+        else:
+            self.search_intent_flag = True
+        self._transition_based_on_intent()
+
+    def _process_intent_response(self, intent_response):
+        intent = intent_response.get("intent", "normal_search")
+        self.entity_slot = intent_response.get("slot", [])
+        if intent == "reject_search":
+            self.reject_intent_flag = True
+            self.search_intent_flag = False
+        elif intent == "agent_search":
+            self.agent_intent_flag = True
+            if self.context.agent_cot_flag:
                 self.search_intent_flag = False
-            elif intent == "agent_search":
-                self.agent_intent_flag = True
-                if self.context.agent_cot_flag:
-                    self.search_intent_flag = False
-                else:
-                    self.search_intent_flag = True
-                    self.agent_intent_flag = False
-            elif intent == "knowledge_search":
-                self.knowledge_search_flag = True
-                self.search_intent_flag = False
-                self.agent_intent_flag = False
             else:
                 self.search_intent_flag = True
+                self.agent_intent_flag = False
+        elif intent == "knowledge_search":
+            self.knowledge_search_flag = True
+            self.search_intent_flag = False
+            self.agent_intent_flag = False
         else:
             self.search_intent_flag = True
 
+    def _transition_based_on_intent(self):
         if self.reject_intent_flag:
             self.transition(QueryState.REJECT_INTENT)
         elif self.knowledge_search_flag:
@@ -196,6 +200,7 @@ class QueryStateMachine:
         else:
             self.answer.query_intent = "normal_search"
         self.transition(QueryState.ENTITY_RETRIEVAL)
+
 
     def handle_reject_intent(self):
         self.answer.query = self.context.search_box
@@ -216,50 +221,51 @@ class QueryStateMachine:
         # Handle entity selection
         self.transition(QueryState.EXECUTE_QUERY)
 
-    def handle_execute_query(self, database_profile, sql):
-        # Execute the SQL query
-        if sql == "":
-            sql_execute_result = {"data": pd.DataFrame(), "sql": sql, "status_code": 500,
-                                  "error_info": "The SQL is empty."}
-        else:
-            sql_execute_result = get_sql_result_tool(database_profile, sql)
+    def handle_execute_query(self):
+        sql = self.intent_search_result.get("sql", "")
+        sql_execute_result = self._execute_sql(sql)
         self.intent_search_result["sql_execute_result"] = sql_execute_result
         if self.context.data_with_analyse and sql_execute_result["status_code"] == 200:
             self.transition(QueryState.ANALYZE_DATA)
         else:
             self.transition(QueryState.ERROR)
 
+    def _execute_sql(self, sql):
+        if sql == "":
+            return {"data": pd.DataFrame(), "sql": sql, "status_code": 500, "error_info": "The SQL is empty."}
+        return get_sql_result_tool(self.context.database_profile, sql)
+
     def handle_sql_auto_correction(self):
         # Handle SQL auto correction
         response = text_to_sql(self.context.database_profile['tables_info'],
-                                   self.context.database_profile['hints'],
-                                   self.context.database_profile['prompt_map'],
-                                   self.context.query_rewrite,
-                                   model_id=self.context.model_type,
-                                   sql_examples=self.normal_search_qa_retrival,
-                                   ner_example=self.normal_search_entity_slot,
-                                   dialect=self.context.database_profile['db_type'],
-                                   model_provider=None,
-                                   additional_info='''\n NOTE: when I try to write a SQL <sql>{sql_statement}</sql>, I got an error <error>{error}</error>. Please consider and avoid this problem. '''.format(
-                                       sql_statement=self.intent_search_result["sql_execute_result"]["sql"],
-                                       error=self.intent_search_result["sql_execute_result"]["error_info"]))
+                               self.context.database_profile['hints'],
+                               self.context.database_profile['prompt_map'],
+                               self.context.query_rewrite,
+                               model_id=self.context.model_type,
+                               sql_examples=self.normal_search_qa_retrival,
+                               ner_example=self.normal_search_entity_slot,
+                               dialect=self.context.database_profile['db_type'],
+                               model_provider=None,
+                               additional_info='''\n NOTE: when I try to write a SQL <sql>{sql_statement}</sql>, I got an error <error>{error}</error>. Please consider and avoid this problem. '''.format(
+                                   sql_statement=self.intent_search_result["sql_execute_result"]["sql"],
+                                   error=self.intent_search_result["sql_execute_result"]["error_info"]))
 
         regen_sql = get_generated_sql(response)
         self.intent_search_result["sql"] = regen_sql
         self.intent_search_result["response"] = response
         self.transition(QueryState.EXECUTE_QUERY)
 
-
     def handle_analyze_data(self):
         # Analyze the data
-        search_intent_analyse_result = data_analyse_tool(self.context.model_type, self.context.database_profile['prompt_map'],
-                                                             self.context.query_rewrite,
-                                                             self.intent_search_result["sql_execute_result"]["data"].to_json(
-                                                                 orient='records',
-                                                                 force_ascii=False), "query")
+        search_intent_analyse_result = data_analyse_tool(self.context.model_type,
+                                                         self.context.database_profile['prompt_map'],
+                                                         self.context.query_rewrite,
+                                                         self.intent_search_result["sql_execute_result"][
+                                                             "data"].to_json(
+                                                             orient='records',
+                                                             force_ascii=False), "query")
         self.answer.sql_search_result.data_analyse = search_intent_analyse_result
         self.transition(QueryState.COMPLETE)
-
 
     def handle_agent_analyze_data(self):
         # Analyze the data
