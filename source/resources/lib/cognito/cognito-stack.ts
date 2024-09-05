eeb47f16ed64fbd73bb3160010042c4154d0b8b0
Pinyu Su <pinyusu@amazon.com>
2024-09-05 16:45:49 +0800

    support specifying bedrock region

--- source/resources/lib/cognito/cognito-stack.ts
+++ source/resources/lib/cognito/cognito-stack.ts
@@ -2,19 +2,35 @@ import * as cdk from 'aws-cdk-lib/core';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
 
+interface CognitoStackProps extends cdk.StackProps {
+    sign_in_aliases_username?: boolean;
+}
 export class CognitoStack extends cdk.Stack {
     public readonly userPoolId: string;
     public readonly userPoolClientId: string;
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props?: CognitoStackProps) {
         super(scope, id, props);
+
+        // help me fix following code
+
+        let signInAlisesOption = undefined
+        let signInCaseSensitiveOption = undefined
+        if (props?.sign_in_aliases_username) {
+            signInAlisesOption =  { email: true, username: true, preferredUsername: true }
+            signInCaseSensitiveOption = false
+        } else {
+            signInAlisesOption =  { email: true }
+            signInCaseSensitiveOption = true // use default value
+        }
 
         // Create a Cognito User Pool
         const userPool = new cognito.UserPool(this, 'GenBiUserPool', {
-        userPoolName: 'GenBiUserPool',
-        selfSignUpEnabled: true,
-        signInAliases: { email: true },
-        autoVerify: { email: true },
-        passwordPolicy: {
+            userPoolName: 'GenBiUserPool',
+            selfSignUpEnabled: true,
+            signInAliases: signInAlisesOption,
+            signInCaseSensitive: signInCaseSensitiveOption,
+            autoVerify: { email: true },
+            passwordPolicy: {
             minLength: 8,
             requireUppercase: false,
             requireLowercase: true,
