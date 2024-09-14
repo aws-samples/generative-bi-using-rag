5047599059b7c5fa7b6f73da07b5ff928e2583b4
Pinyu Su <pinyusu@amazon.com>
2024-09-14 16:04:42 +0800

    add sql and update

--- application/pages/8_📚_Agent_Cot_Management.py
+++ application/pages/8_📚_Agent_Cot_Management.py
@@ -94,7 +94,7 @@ def main():
 
     if current_profile is not None:
         if st.session_state.cot_refresh_view or st.session_state["cot_sample_search"][current_profile] is None:
-            st.session_state["cot_sample_search"][current_profile] = VectorStore.get_all_samples(current_profile)
+            st.session_state["cot_sample_search"][current_profile] = VectorStore.get_all_agent_cot_samples(current_profile)
             st.session_state.sql_refresh_view = False
 
     tab_view, tab_add, tab_search = st.tabs(['View Samples', 'Add New Sample', 'Sample Search'])
@@ -113,19 +113,21 @@ def main():
 
         with tab_add:
             if current_profile is not None:
-                query = st.text_input('Query', key='index_question')
-                comment = st.text_area('Comment', key='index_answer', height=300)
+                with st.form(key='cot_add_form'):
+                    query = st.text_input('Query', key='index_question')
+                    comment = st.text_area('Comment', key='index_answer', height=300)
 
-                if st.button('Submit', type='primary'):
-                    if len(query) > 0 and len(comment) > 0:
-                        VectorStore.add_agent_cot_sample(current_profile, query, comment)
-                        st.success('Sample added')
-                        time.sleep(2)
-                        # del st.session_state['index_question']
-                        # del st.session_state['index_answer']
-                        st.rerun()
-                    else:
-                        st.error('please input valid question and answer')
+                    if st.form_submit_button('Add Cot Info', type='primary'):
+                        if len(query) > 0 and len(comment) > 0:
+                            VectorStore.add_agent_cot_sample(current_profile, query, comment)
+                            st.success('Sample added')
+                            st.success('Update Index')
+                            with st.spinner('Update Index ...'):
+                                time.sleep(2)
+                            st.session_state["cot_sample_search"][current_profile] = VectorStore.get_all_agent_cot_samples(current_profile)
+                            st.rerun()
+                        else:
+                            st.error('please input valid question and answer')
 
         with tab_search:
             if current_profile is not None:
