638643aed7629247899a7a6dac21995627762565
Pinyu Su <pinyusu@amazon.com>
2024-09-17 16:31:57 +0800

    update llama3

--- application/pages/1_🌍_Generative_BI_Playground.py
+++ application/pages/1_🌍_Generative_BI_Playground.py
@@ -207,7 +207,7 @@ def main():
         st.session_state.samaker_model = ModelManagement.get_all_models()
 
 
-    model_ids = ['meta.llama3-1-70b-instruct-v1' ,'anthropic.claude-3-sonnet-20240229-v1:0', 'anthropic.claude-3-5-sonnet-20240620-v1:0',
+    model_ids = ['meta.llama3-1-70b-instruct-v1:0' ,'anthropic.claude-3-sonnet-20240229-v1:0', 'anthropic.claude-3-5-sonnet-20240620-v1:0',
                  'anthropic.claude-3-opus-20240229-v1:0',
                  'anthropic.claude-3-haiku-20240307-v1:0', 'mistral.mixtral-8x7b-instruct-v0:1']
 
