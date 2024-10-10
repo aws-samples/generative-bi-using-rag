0d402bc2878e1b4aad614219e0e321b1678e0e8a
Pinyu Su <pinyusu@amazon.com>
2024-10-10 16:53:43 +0800

    update v1.8.0

---
+++ application/import_sql_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+wget https://raw.githubusercontent.com/harryho/db-samples/refs/heads/master/mysql/northwind.sql
+
+cp northwind.sql initial_data/
+
+docker exec nlq-mysql sh -c "mysql -u root -ppassword -D llm  < /opt/data/northwind.sql"
