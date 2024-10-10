7609f919eeaed7bcee5fe57b4edc26a3fae8a9d3
Pinyu Su <pinyusu@amazon.com>
2024-10-10 16:55:20 +0800

    update v1.8.0

--- application/install_genbi.sh
+++ application/install_genbi.sh
@@ -2,7 +2,7 @@
 
 # 执行第一个脚本
 echo "Running environment_preparation.sh..."
-./environment_preparation.sh
+bash environment_preparation.sh
 
 # 检查上一个脚本是否成功执行
 if [ $? -eq 0 ]; then
@@ -14,7 +14,7 @@ fi
 
 # 执行第二个脚本
 echo "Running docker-compose-build.sh..."
-./docker-compose-build.sh
+bash docker-compose-build.sh
 
 # 检查上一个脚本是否成功执行
 if [ $? -eq 0 ]; then
@@ -26,7 +26,7 @@ fi
 
 # 执行第三个脚本
 echo "Running import_sql_data.sh..."
-./import_sql_data.sh
+bash import_sql_data.sh
 
 sleep 20
 
